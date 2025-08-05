2a4883d62b493608100825600387319f66552cdc
Stephen R <56372633+c5473c4c@users.noreply.github.com>
2025-08-05 14:54:28 -0400

    remove old Jwave-pro relics

--- morphiq-core/src/main/java/com/prophetizo/wavelets/WaveletType.java
+++ morphiq-core/src/main/java/com/prophetizo/wavelets/WaveletType.java
@@ -63,31 +63,6 @@ public enum WaveletType {
         return displayName;
     }
 
-    /**
-     * Creates a JWave Wavelet instance for this type.
-     * @return a new Wavelet instance
-     */
-    /*public Wavelet createWavelet() {
-        switch (this) {
-            case HAAR:
-                return new Haar1();
-            case DAUBECHIES2:
-                return new Daubechies2();
-            case DAUBECHIES4:
-                return new Daubechies4();
-            case DAUBECHIES6:
-                return new Daubechies6();
-            case SYMLET4:
-                return new Symlet4();
-            case SYMLET8:
-                return new Symlet8();
-            case COIFLET3:
-                return new Coiflet3();
-            default:
-                logger.warn("Unknown wavelet type: {}, defaulting to Daubechies4", this);
-                return new Daubechies4();
-        }
-    }*/
 
     /**
      * Parses a wavelet type from a string representation.
